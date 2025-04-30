637d2e4235d8e6a006e795d910db40b43bc910b8
Bobozia <sokoliukbohdan@gmail.com>
2025-04-30 22:17:20 +0200

    Refactor DatabaseContext to improve readability

--- backend/Data/DatabaseContext.cs
+++ backend/Data/DatabaseContext.cs
@@ -3,65 +3,58 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Helpers;
 
-namespace Data
+namespace Data;
+
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : IdentityDbContext<User>(options)
 {
-  public class DatabaseContext : IdentityDbContext<User>
+  public DbSet<Follow> Follows { get; set; }
+  public DbSet<ProfileComment> ProfileComments { get; set; }
+
+  public DbSet<Scrobble> Scrobbles { get; set; }
+
+  public DbSet<Song> Songs { get; set; }
+  public DbSet<SongComment> SongComments { get; set; }
+  public DbSet<SongRating> SongRatings { get; set; }
+  public DbSet<FavouriteSong> FavouriteSongs { get; set; }
+
+  public DbSet<Album> Albums { get; set; }
+  public DbSet<AlbumComment> AlbumComments { get; set; }
+  public DbSet<AlbumRating> AlbumRatings { get; set; }
+
+  public DbSet<Artist> Artists { get; set; }
+  public DbSet<ArtistComment> ArtistComments { get; set; }
+  public DbSet<ArtistRating> ArtistRatings { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    public DbSet<Follow> Follows { get; set; }
-    public DbSet<ProfileComment> ProfileComments { get; set; }
+    base.OnConfiguring(optionsBuilder);
+  }
 
-    public DbSet<Scrobble> Scrobbles { get; set; }
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
 
-    public DbSet<Song> Songs { get; set; }
-    public DbSet<SongComment> SongComments { get; set; }
-    public DbSet<SongRating> SongRatings { get; set; }
-    public DbSet<FavouriteSong> FavouriteSongs { get; set; }
+    modelBuilder.Entity<Follow>()
+      .HasOne(f => f.Followed)
+      .WithMany(u => u.Followers)
+      .HasForeignKey(f => f.Id_Followed);
 
-    public DbSet<Album> Albums { get; set; }
-    public DbSet<AlbumComment> AlbumComments { get; set; }
-    public DbSet<AlbumRating> AlbumRatings { get; set; }
+    new ScrobbleMap(modelBuilder.Entity<Scrobble>());
 
-    public DbSet<Artist> Artists { get; set; }
-    public DbSet<ArtistComment> ArtistComments { get; set; }
-    public DbSet<ArtistRating> ArtistRatings { get; set; }
+    new FollowMap(modelBuilder.Entity<Follow>());
+    new ProfileCommentMap(modelBuilder.Entity<ProfileComment>());
 
-    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
-    {
-    }
+    new SongMap(modelBuilder.Entity<Song>());
+    new SongCommentMap(modelBuilder.Entity<SongComment>());
+    new SongRatingMap(modelBuilder.Entity<SongRating>());
+    new FavouriteSongMap(modelBuilder.Entity<FavouriteSong>());
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      base.OnConfiguring(optionsBuilder);
-    }
+    new AlbumMap(modelBuilder.Entity<Album>());
+    new AlbumCommentMap(modelBuilder.Entity<AlbumComment>());
+    new AlbumRatingMap(modelBuilder.Entity<AlbumRating>());
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-      base.OnModelCreating(modelBuilder);
-
-      modelBuilder.Entity<Follow>()
-        .HasOne(f => f.Followed)
-        .WithMany(u => u.Followers)
-        .HasForeignKey(f => f.Id_Followed);
-
-      new ScrobbleMap(modelBuilder.Entity<Scrobble>());
-
-      new FollowMap(modelBuilder.Entity<Follow>());
-      new ProfileCommentMap(modelBuilder.Entity<ProfileComment>());
-
-      new SongMap(modelBuilder.Entity<Song>());
-      new SongCommentMap(modelBuilder.Entity<SongComment>());
-      new SongRatingMap(modelBuilder.Entity<SongRating>());
-      new FavouriteSongMap(modelBuilder.Entity<FavouriteSong>());
-
-      new AlbumMap(modelBuilder.Entity<Album>());
-      new AlbumCommentMap(modelBuilder.Entity<AlbumComment>());
-      new AlbumRatingMap(modelBuilder.Entity<AlbumRating>());
-
-      new ArtistMap(modelBuilder.Entity<Artist>());
-      new ArtistCommentMap(modelBuilder.Entity<ArtistComment>());
-      new ArtistRatingMap(modelBuilder.Entity<ArtistRating>());
-    }
-
-
+    new ArtistMap(modelBuilder.Entity<Artist>());
+    new ArtistCommentMap(modelBuilder.Entity<ArtistComment>());
+    new ArtistRatingMap(modelBuilder.Entity<ArtistRating>());
   }
 }
