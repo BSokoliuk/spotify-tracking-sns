d0c93610ff204f5d57708181674317f9da7d82ab
Bobozia <sokoliukbohdan@gmail.com>
2025-05-05 17:37:23 +0200

    Update LoginResponse class

--- backend/DTOs/Auth/LoginResponse.cs
+++ backend/DTOs/Auth/LoginResponse.cs
@@ -5,4 +5,6 @@ public class LoginResponse
     public bool Success { get; set; }
     public string Message { get; set; } = string.Empty;
     public string Id { get; set; } = string.Empty;
+    public bool IsAdmin { get; set; } = false;
+    public string Token { get; set; } = string.Empty;
 }
